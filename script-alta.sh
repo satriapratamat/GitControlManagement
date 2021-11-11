77b7d70262ed1ed4964075b58c06452449896ac0
Satria Pratama Taruna <satriapratamat7@gmail.com>
2021-11-11 14:09:49 +0700

    first commit

---
+++ script-alta.sh
@@ -0,0 +1,2 @@
+echo Git Management Control
+echo Hello World
---
+++ script-alta2.sh
@@ -0,0 +1,9 @@
+cat script-alta2.sh
+angka1=25
+angka2=5
+
+#operasi perkalian dua bilangan
+sum=$((angka1*angka2))
+echo maka $angka1 * $angka2 = $sum
+
+
